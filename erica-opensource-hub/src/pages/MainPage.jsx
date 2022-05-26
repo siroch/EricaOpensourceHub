450658506291381b10d5d0a8f9dfe4f1476206a0
siroch <habizero@gmail.com>
2022-05-26 17:44:51 +0900

    feat: 메인화면 데이터 기본(1)

--- erica-opensource-hub/src/pages/MainPage.jsx
+++ erica-opensource-hub/src/pages/MainPage.jsx
@@ -1,5 +1,27 @@
+import "./MainPage.css"
+import ProjectData from "./project_item_test.json"
+
 function MainPage() {
-  return <>메인페이지</>;
+  const project_list = ProjectData.project.map((project_data) => {
+    return JSON.stringify(project_data)
+  })
+
+  const course_list = ProjectData.course.map((course_data) => {
+    return JSON.stringify(course_data)
+  })
+  return (
+    <div>
+      <div>
+        EOH
+      </div><div>
+        {course_list}
+      </div>
+      <div>
+        {project_list}
+      </div>
+      <button>글쓰기</button>
+    </div>
+  );
 }
 
 export default MainPage;
