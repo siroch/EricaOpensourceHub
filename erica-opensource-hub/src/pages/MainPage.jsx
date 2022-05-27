b14578e51017b2ac181547c8eb4d84e4aa73ac70
siroch <habizero@gmail.com>
2022-05-27 10:48:56 +0900

    fix: 데이터구조 수정(임시)

--- erica-opensource-hub/src/pages/MainPage.jsx
+++ erica-opensource-hub/src/pages/MainPage.jsx
@@ -1,26 +1,39 @@
 import "./MainPage.css"
 import ProjectData from "./project_item_test.json"
+import CourseData from "./course_item_test.json"
 
 function MainPage() {
   const project_list = ProjectData.project.map((project_data) => {
     return JSON.stringify(project_data)
   })
 
-  const course_list = ProjectData.course.map((course_data) => {
+  const course_list = CourseData.course.map((course_data) => {
     return JSON.stringify(course_data)
   })
   return (
     <div>
       <div>
         EOH
-      </div><div>
-        {course_list}
-      </div>
-      <div>
-        {project_list}
       </div>
       <button>글쓰기</button>
       <button>로그인</button>
+      <ul>
+        {CourseData.course.map((course_data) => {
+          <li key={course_data.id}>
+            {[course_data.college, course_data.department
+            ]}
+          </li>
+        })}
+      </ul>
+      <ul>
+        {ProjectData.project.map((project_data) => (
+          <li key={project_data.project_id}>
+            {[project_data.title, project_data.createdate, project_data.like, project_data.view, 
+              project_data.team_proposal, project_data.course, project_data.hashtag
+            ]}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
