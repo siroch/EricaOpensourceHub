ff13603bdcc12e88ab311ceae797d466e7c01164
siroch <habizero@gmail.com>
2022-05-30 20:21:54 +0900

    test: 해당 프로젝트에 맞는 업데이트 목록 로드

--- erica-opensource-hub/src/pages/PostUpdateForm.jsx
+++ erica-opensource-hub/src/pages/PostUpdateForm.jsx
@@ -1,21 +1,18 @@
 import "./PostUpdateForm.css"
 import ProjectData from "../json/project_item_test.json"
 
-function UpdateLog({data}){ // 핀 형태로 변경 예정, 클릭시 project id와 version을 넘겨줌, 호버시 커밋메세지,커밋한유저 정보 뜸
-  return (
-  <button>
-    {[data.owner, data.title, data.project_version]}
-  </button>
-  );
-}
-
 function PostUpdateForm() {
   return (
     <div>
       <ul>
         {ProjectData.project.map((project_data) => (
           project_data.project_id === 1 // 프로젝트 id로 판별
-          ? <UpdateLog data={project_data} key={project_data.owner}/> 
+          ? <div class="help-tip">
+              <p>edit date: {project_data.editdate}<br />
+              user id: {project_data.owner}<br />
+              commit message: {project_data.commit_message}<br />
+              project_version: {project_data.project_version}</p>
+            </div>
           : <></>
         ))}
       </ul>
