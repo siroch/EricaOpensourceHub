8e33bf2de8a5253f0df411eb347678f94144acc5
siroch <habizero@gmail.com>
2022-06-01 17:03:38 +0900

    feat: 버전에 맞게 데이터 전달, 네비게이터

--- erica-opensource-hub/src/pages/PostUpdateForm.jsx
+++ erica-opensource-hub/src/pages/PostUpdateForm.jsx
@@ -1,9 +1,15 @@
 import "./PostUpdateForm.css"
 import ProjectData from "../json/project_item_test.json"
+import { useNavigate } from "react-router-dom";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 function PostUpdateForm() {
+  const navigate = useNavigate();
+
+  const goToMain = (id) => {
+    navigate("/post/:"+String(id));
+  };
   return (
     <div class="test">
       <ul>
@@ -16,7 +22,9 @@ function PostUpdateForm() {
               contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', width: '45vh', left:'25vh', cursor: 'pointer' }}
               contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
               iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', cursor: 'pointer' }}
-              icon={""}>
+              icon={""}
+              iconOnClick={()=>goToMain(project_data.project_version)}
+              onTimelineElementClick={()=>goToMain(project_data.project_version)}>
               <h3 className="vertical-timeline-element-title">{project_data.commit_message}</h3>
               <h4 className="vertical-timeline-element-subtitle">{project_data.commit_user}</h4>
               <p>
